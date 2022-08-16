950de4cf8514d5badc099f797acf2649449030f3
uyn-nn <kieuuyen17@gmail.com>
2022-08-16 14:38:35 +0700

    Checkbox, count item to the list

---
+++ todo/lib/main.dart
@@ -0,0 +1,238 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: const MaterialApp(
+        home: TasksScreen(),
+      ),
+    );
+  }
+}
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            isScrollControlled: true,
+            context: context,
+            builder: (context) => SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: AddTaskScreen((newTaskTitle) {
+                  // setState(() {
+                  //   tasks.add(Task(name: newTaskTitle));
+                  //   Navigator.pop(context);
+                  // });
+                }),
+              ),
+            ),
+          );
+        },
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
+      ),
+      backgroundColor: Colors.indigo[400],
+      body: Container(
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 20,
+          right: 20,
+          bottom: 80,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: const [
+                Icon(
+                  Icons.playlist_add_check,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                SizedBox(width: 20),
+                Text(
+                  'ToDoList',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              '${Provider.of<TaskData>(context).tasks.length} Tasks',
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                child: TasksList(),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AddTaskScreen extends StatelessWidget {
+  final Function addTaskCallback;
+
+  AddTaskScreen(this.addTaskCallback);
+
+  @override
+  Widget build(BuildContext context) {
+    String? newTaskTitle;
+    return AlertDialog(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      title: const Text('New Todo'),
+      content: TextField(
+        autofocus: true,
+        textAlign: TextAlign.center,
+        onChanged: (newText) {
+          newTaskTitle = newText;
+        },
+        decoration: const InputDecoration(
+          hintText: 'Enter something to do...',
+          contentPadding: EdgeInsets.all(16.0),
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text('Cancel'),
+        ),
+        FlatButton(
+          onPressed: () {
+            Provider.of<TaskData>(context, listen: false)
+                .addTask(newTaskTitle!);
+            Navigator.pop(context);
+          },
+          child: const Text('Add'),
+        ),
+      ],
+    );
+  }
+}
+
+class TaskData extends ChangeNotifier {
+  List<Task> tasks = [
+    Task(name: 'Write small apps'),
+    Task(name: 'Make todo list'),
+    Task(name: 'Commit changes and push to Github'),
+  ];
+
+  void addTask(String newTaskTitle) {
+    tasks.add(Task(name: newTaskTitle));
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.doneChange();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    tasks.remove(task);
+    notifyListeners();
+  }
+}
+
+class Task {
+  final String name;
+  bool isDone;
+
+  Task({required this.name, this.isDone = false});
+
+  void doneChange() {
+    isDone = !isDone;
+  }
+}
+
+class TasksList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<TaskData>(
+      builder: (context, taskData, child) {
+        return ListView.builder(
+          itemCount: taskData.tasks.length,
+          itemBuilder: (context, index) {
+            return TaskTile(
+              isChecked: taskData.tasks[index].isDone,
+              taskTitle: taskData.tasks[index].name,
+              checkboxChange: (newValue) {
+                taskData.updateTask(taskData.tasks[index]);
+              },
+              listTileDelete: () {
+                taskData.deleteTask(taskData.tasks[index]);
+              },
+            );
+          },
+        );
+      },
+    );
+  }
+}
+
+class TaskTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final void Function(bool?) checkboxChange;
+  final void Function() listTileDelete;
+
+  // ignore: use_key_in_widget_constructors
+  const TaskTile(
+      {required this.isChecked,
+      required this.taskTitle,
+      required this.checkboxChange,
+      required this.listTileDelete});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.teal[400],
+        value: isChecked,
+        onChanged: checkboxChange,
+      ),
+      onLongPress: listTileDelete,
+    );
+  }
+}
